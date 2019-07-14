95eff75ada0e39160ea21b8978dd55f862f7b1f3
zachrybaker <zachrybaker@gmail.com>
2019-07-13 22:42:08 -0500

    Note that firebase config is not secret

--- ClientAppConfig.ts
+++ ClientAppConfig.ts
@@ -1,14 +1,18 @@
-﻿
+﻿/* 
+From https://firebase.google.com/docs/web/setup?authuser=0#config-object
+Note: The Firebase config object contains unique, but non-secret identifiers for your Firebase project. 
+Visit Understand Firebase Projects to learn more about this config object.
+*/
 export default class ClientAppConfig {
     // Drop hardcoded configuration (that doesn't need to be emitted by the web API)
     firebaseConfig = {
-        apiKey: "AIzaSyAAHVkUOgj5IBvMW3ee2kIAxgasEghIx5w",
+        apiKey: "[my-non-secret-api-key]",
         authDomain: "opensongweb-auth.firebaseapp.com",
         databaseURL: "https://opensongweb-auth.firebaseio.com",
         projectId: "opensongweb-auth",
         storageBucket: "",
-        messagingSenderId: "84624904344",
-        appId: "1:84624904344:web:c7dabf3263c5ccc5"
+        messagingSenderId: "[my non-secret messaging sender id]S",
+        appId: "[my non-secret app id]"
     };
 
 };
